f0ea3ef0084e3d1a922e87639ce9ce76ca52690e
mckinly <mckinly.springer@gmail.com>
2020-04-04 14:25:13 -0500

    Add str repr

--- agrisight/geosight/models.py
+++ agrisight/geosight/models.py
@@ -4,8 +4,15 @@ from django.db import models
 class Location(models.Model):
     longitude = models.CharField(max_length=200)
     latitude = models.CharField(max_length=200)
+    location_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location_text
 
 
 class Choice(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
