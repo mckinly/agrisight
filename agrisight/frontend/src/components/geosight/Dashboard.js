35fc3696a8f5db2b148265c20044bfc7d77ca961
mckinly <mckinly.springer@gmail.com>
2020-04-08 22:45:04 -0500

    Add base components

---
+++ agrisight/frontend/src/components/geosight/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Form from './Form'
+import Geosight from './Geosight'
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Form />
+            <Geosight />
+        </Fragment>
+    )
+}
---
+++ agrisight/frontend/src/components/geosight/Geosight.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Geosight extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Geosight main view</h1>
+            </div>
+        )
+    }
+}
+
+export default Geosight
