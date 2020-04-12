11f19d494788b054bbd864d50a247325c16fd7bf
mckinly <mckinly.springer@gmail.com>
2020-04-12 17:34:42 -0500

    Add skeleton for google satellite view

--- agrimanager/agrisight/urls.py
+++ agrimanager/agrisight/urls.py
@@ -2,5 +2,6 @@ from rest_framework import routers
 from .api import AgrisightViewSet
 
 router = routers.DefaultRouter()
+
 router.register("api/agrisight", AgrisightViewSet, "agrisight")
 urlpatterns = router.urls
