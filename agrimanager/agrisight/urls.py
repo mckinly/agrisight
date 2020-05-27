84e668ab9fb00ef20b36caaa9b84e34f0b047236
mckinly <mckinly.springer@gmail.com>
2020-05-27 11:22:18 -0500

    Test automation

--- agrimanager/agrisight/urls.py
+++ agrimanager/agrisight/urls.py
@@ -4,5 +4,4 @@ from .api import AgrisightViewSet
 router = routers.DefaultRouter()
 
 router.register("api/agrisight", AgrisightViewSet, "agrisight")
-print(f"!!!!!!!!!!!!!URLS: {router.urls}")
 urlpatterns = router.urls
