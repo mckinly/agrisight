481c9358d405d34845549fc4ecd809cbb7239886
mckinly <mckinly@mckinlys-MacBook-Pro.local>
2020-12-04 10:14:56 -0800

    Add load test step

--- test/performance/locustfile.py
+++ test/performance/locustfile.py
@@ -1,6 +1,21 @@
 import time
-from locust import HttpUser, task
-import requests
+import logging
+from locust import HttpUser, task, events
+
+
+@events.quitting.add_listener
+def _(environment, **kw):
+    if environment.stats.total.fail_ratio > 0.2:
+        logging.error("Test failed due to failure ratio > 20%")
+        environment.process_exit_code = 1
+    elif environment.stats.total.avg_response_time > 200:
+        logging.error("Test failed due to average response time ratio > 200 ms")
+        environment.process_exit_code = 1
+    elif environment.stats.total.get_response_time_percentile(0.95) > 800:
+        logging.error("Test failed due to 95th percentile response time > 800 ms")
+        environment.process_exit_code = 1
+    else:
+        environment.process_exit_code = 0
 
 
 class QuickstartUser(HttpUser):
@@ -8,10 +23,10 @@ class QuickstartUser(HttpUser):
     def hello_world(self):
         self.client.get("/#/dashboard")
 
-    @task(3)
-    def view_item(self):
-        pass
-
-    def on_start(self):
-        self.client.post("/api/auth/login", json={"username": "test", "password": "password123"})
-        pass
+    # @task(3)
+    # def view_item(self):
+    #     pass
+    #
+    # def on_start(self):
+    #     self.client.post("/api/auth/login", json={"username": "test", "password": "password123"})
+    #     pass
