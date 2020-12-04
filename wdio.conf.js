25141828d37744ecc0ab7a387167a8d269c0c24b
mckinly <mckinly@mckinlys-MacBook-Pro.local>
2020-12-04 07:49:51 -0800

    Update browser url and wdio config

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,7 @@ exports.config = {
         browserName: 'chrome',
         acceptInsecureCerts: true,
         'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu'],
+            args: ['headless', 'window-size=1920,1080', 'disable-gpu'],
         },
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
