86c489804cc02d74d9831f4135318c16b5254143
mckinly <mckinly@mckinlys-MacBook-Pro.local>
2020-12-04 10:47:22 -0800

    Experimental wdio conf chrome options

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,7 @@ exports.config = {
         browserName: 'chrome',
         acceptInsecureCerts: true,
         'goog:chromeOptions': {
-            args: ['--headless', '--window-size=1920,1080', '--disable-gpu', '--disable-dev-shm-usage', '--no-sandbox'],
+            args: ['--headless', '--window-size=1920,1080', '--disable-gpu', '--disable-dev-shm-usage', '--no-sandbox', "--proxy-server='direct://", "--proxy-bypass-list=*"],
         },
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
